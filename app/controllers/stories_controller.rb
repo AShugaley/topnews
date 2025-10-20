20aa3d4b9099d1fbca25ec52884b4815d809e885
Alex Shugaley <shugaley@Alexs-Air.cable.rcn.com>
2025-10-20 14:11:43 -0400

    desc-sort

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,7 +2,7 @@ class StoriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @stories = Story.page(params[:page]).per(20)
+    @stories = Story.order(created_at: :desc).page(params[:page]).per(20)
   end
 
   def upvote
